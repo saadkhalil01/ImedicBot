14d99dd6ace6a78059125d1ce89d29b144d3b900
Saad Khalil <saadkhalil9999@gmail.com>
2023-08-05 20:53:33 +0500

    ok

--- components/InputField.jsx
+++ components/InputField.jsx
@@ -1,42 +1,54 @@
-import { View, Image,TextInput, StyleSheet, TouchableOpacity } from 'react-native'
+import { View, Image, TextInput, StyleSheet, TouchableWithoutFeedback, secureTextEntry, Alert } from 'react-native'
 import { useState } from 'react';
 import React from 'react'
 
 export default function InputField({ name }) {
-    const [eye,setEye] = useState(true);
+    const [eye, setEye] = useState(false);
+    const [input, setInput] = useState('');
+    const [hide, Show] = useState(true);
 
     return (
-        
-            <TextInput style={styles.mainContainer}
+        <>
+          <View style={styles.mainContainer}>
+            <TextInput style={styles.TextStyle}
                 placeholder={name}
-                placeholderTextColor='grey'
+                placeholderTextColor='black'
+                onChangeText={setInput}
+                value={input}
+                secureTextEntry={hide}
             />
-            // <TouchableOpacity onPress={()=>setEye(!eye)}>
-            //     <Image style={styles.imageShape }
-            //     source={
-            //         name==='password' || name ==='confirm password'?
-            //           require('..\assets\Image\eye.png')
-            //         :
-            //           require('..\assets\Image\closeEye.jpeg')    
-            //     }/>
-            // </TouchableOpacity>    
-        
+            <TouchableWithoutFeedback onPress={() => {setEye(!eye);Show(!hide)}}>
+                <Image style={styles.imageShape}
+                    source={
+                        ((name === 'password' || name === 'confirm password') && eye === true) ?
+                            require('../assets/Image/eye.png')
+                            :
+                            require('../assets/Image/closeEye.png')
+                    } />
+            </TouchableWithoutFeedback>
+         </View>
+        </>
     )
 }
 const styles = StyleSheet.create({
     mainContainer: {
-        width: '90%',
+        width: '80%',
         height: 60,
+        flexDirection: 'row',
         backgroundColor: '#FFFFFF',
         borderRadius: 15,
         alignSelf: 'center',
         elevation: 2,
-        paddingLeft: 20,
-        justifyContent:'space-between'
+        padding: 10,
+        justifyContent: 'space-between',
+        alignItems: 'center'
     },
-    imageShape:{
-        height:40,
-        width:40
+    imageShape: {
+        height: 25,
+        width: 25
+    },
+    TextStyle: {
+        color: 'black',
     }
 
 });
